2e580f51a04248f76552d6b9591dcd12e177d0c8
Christopher Araya <chrarayas@gmail.com>
2020-12-07 16:09:03 -0300

    2nd project a

--- 2-tour/src/App.js
+++ 2-tour/src/App.js
@@ -1,6 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
+
+const url = "https://course-api.netlify.app/api/react-tours-project";
+
+// useEffect(() => {
+//   effect;
+//   return () => {
+//     cleanup;
+//   };
+// }, [input]);
+
 function App() {
-  return <h2>tours project</h2>;
+	const [isLoading, setIsLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+  if (isLoading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+  return <Tours />;
 }
 
 export default App;
